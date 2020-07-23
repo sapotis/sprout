8e8f95fb9e98761487809c018435536d5375bf4e
blinkous <saradhani1@gmail.com>
2020-07-23 18:26:26 -0400

    added schema for table

--- server/migrations/20200722194538_create-user.js
+++ server/migrations/20200722194538_create-user.js
@@ -1,8 +1,11 @@
+export const up = (knex) =>
+  knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.unique("username");
+    table.unique("email");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string("first_name");
+    table.string("last_name");
+  });
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+export const down = (knex) => knex.schema.dropTable("users");
--- server/migrations/20200722194547_create-goal.js
+++ server/migrations/20200722194547_create-goal.js
@@ -1,8 +1,15 @@
+export const up = (knex) =>
+  knex.schema.createTable("goals", (table) => {
+    table.increments("id");
+    table.unique("user_id");
+    table.unique("parent_goal");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.string("name");
+    table.string("description");
+    table.string("category");
+    table.date("end_date");
+    table.boolean("completed");
+    table.string("frequency");
+  });
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+export const down = (knex) => knex.schema.dropTable("goals");
